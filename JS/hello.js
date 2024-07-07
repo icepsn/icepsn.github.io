83bedf8fe2748b3e2e2ff6736f36975caaffcfdf
icepsn <u6411390@au.edu>
2024-07-07 17:57:10 +0700

    First Bootstrap

---
+++ JS/hello.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+const x = 10
+//x = 20
+
+for(var i=0; i<5; i++) {
+    console.log(i)
+}
+
+console.log(i)
---
+++ JS/sum.js
@@ -0,0 +1,22 @@
+L = [1,2,3,4,5,6,7,8,9,10] 
+
+let sum = 0
+for(let i = 0; i<L.length; i++){
+    sum += L[i]
+}
+
+console.log('Loop',sum)
+
+function add(acc,cur){
+    return acc + cur
+}
+
+sum = L.reduce(add)
+console.log('Reducer', sum)
+
+sum = L.reduce((acc,cur) => acc + cur)
+console.log("Arrow", sum)
+
+
+let multi = L.reduce((a,b) => a*b)
+console.log('Multiplication ',multi)
